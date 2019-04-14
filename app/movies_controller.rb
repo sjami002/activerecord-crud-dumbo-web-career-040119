d907667cb58da3a9f4e42ec7fd7ee88e35166afd
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-14 22:01:45 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -76,7 +76,7 @@ def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
   movie = Movie.find_by title: "Wat?"
-  __
+  movie.update
 end
 
 def can_update_multiple_items_at_once
